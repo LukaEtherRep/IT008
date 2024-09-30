691c5bd7557d7366410c9dc6b9d1c612bf794cf4
Nguyen Nhat Huy <154767840+LukaEtherRep@users.noreply.github.com>
2024-09-30 13:36:47 +0700

    Updated 2.1
    Updated this while on school

--- WindowsForm/WindowsForm/2.1/ClsTreeListView.cs
+++ WindowsForm/WindowsForm/2.1/ClsTreeListView.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Management;
+using System.IO;
+
 
 namespace _2._1
 {
@@ -31,6 +33,56 @@ namespace _2._1
                 // Chèn vào Treeview
                 nodeCollection.Add(diskTreeNode);
             }
+        }
+
+        public bool ShowFolderTree(TreeView treeView, TreeNode currentNode)
+        {
+            if(currentNode.Text != "My Computer")
+            {
+                try
+                {
+                    // Kiểm tra đường dẫn
+                    if (Directory.Exists(GetFullPath(currentNode.FullPath)) == false)
+                        MessageBox.Show("Ổ đĩa hoặc thư mục không tồn tại");
+                    else
+                    {
+                        string[] strDirectories = Directory.GetDirectories(GetFullPath(currentNode.FullPath));
+                        // Lấy thông tin thư mục
+                        foreach (string stringDir in strDirectories)
+                        {
+                            string strName = GetName(stringDir);
+                            TreeNode nodeDir;
+                            // Tạo các node cho thư mục
+                            nodeDir = new TreeNode(strName, 0, 0);
+                            currentNode.Nodes.Add(nodeDir);
+                        }
+                    }
+                    return true;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Ổ đĩa hoặc thư mục không tồn tại");
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Bạn không có quyền truy cập vào ổ đĩa hoặc thư mục này");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+            return false;
+        }
+        public string GetFullPath(string strPath)
+        {
+            return strPath.Replace("My Computer\\", "").Replace("\\\\", "\\");
+        }
+        public string GetName(string strPath)
+        {
+            string[] strSplit = strPath.Split('\\');
+            int maxIndex = strSplit.Length;
+            return strSplit[maxIndex - 1];
         }
     }
 }
